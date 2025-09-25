b870ff2bd10ff6f1f046186d6eee8aa7486d8264
varsh1605 <varshini97917@gmail.com>
2025-09-25 11:17:14 +0530

    Add files via upload

---
+++ jam.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Jam {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+		int n2=sc.nextInt();
+		System.out.println(2*n2);
+		}
+		
+		
+		
+		
+
+	}
+
+}
